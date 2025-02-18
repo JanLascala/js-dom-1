e9d5b9f2ab4f1c6b6f3d605035f72712142bf13b
JanLascala <lascala.jan@gmail.com>
2025-02-18 15:28:40 +0100

    Add style css, Issue

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ const whiteBulb = document.getElementById('white_bulb')
 const yelloBulb = document.getElementById('yellow_bulb')
 
 const powerButton = document.querySelector('Switch')
+
+
+
+
+
+
+
+
+
+powerButton.addEventListener('click', function () {
+    console.log('click');
+})
+
