0cbb07a038c80f8333b13ff71be40b17e81741b5
JanLascala <lascala.jan@gmail.com>
2025-02-18 13:46:42 +0100

    Add const in JS

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("work")
+
+const whiteBulb = document.getElementById('white_bulb')
+
+const yelloBulb = document.getElementById('yellow_bulb')
+
+const powerButton = document.querySelector('Switch')
